668a6d66c89969443dfaa7ca6f3d4652d42c58d4
italoengdev <italo_saraiva182@hotmail.com>
2023-05-21 23:16:19 -0300

    try add _document

---
+++ src/pages/_document.js
@@ -0,0 +1,32 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Your other meta tags and external scripts */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-P75DW6P224"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-P75DW6P224');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
